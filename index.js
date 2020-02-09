c9a1120b638b5cbf6d744df4f672dfd175b00779
Ville Marttila <ville.marttila@iki.fi>
2020-02-10 01:38:12 +0200

    tehtävä 3.1 tehty

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+const persons = {
+    "persons": [
+      {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+      }
+    ]
+  }
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
