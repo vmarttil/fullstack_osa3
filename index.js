0e1ff0d7f6db594f3d3b807945b59a53b3eab207
Ville Marttila <ville.marttila@iki.fi>
2020-02-10 20:38:55 +0200

    poistettu node_modules

--- index.js
+++ index.js
@@ -86,6 +86,7 @@ app.post('/api/persons', (request, response, next) => {
     }
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
