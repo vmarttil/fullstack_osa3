7d0571c591bc9e3773e2eb161d8bfe3effc1592d
Ville Marttila <ville.marttila@iki.fi>
2020-02-10 20:52:52 +0200

    tehtävä 3.11 tehty

---
+++ build/precache-manifest.4f257108dcf7e14aca0a0293027cb949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119b416a4d87600e403d4f92f2aa26c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f7f747ff583fda38440",
+    "url": "/static/css/main.4cf20161.chunk.css"
+  },
+  {
+    "revision": "4b18cf36501447135d88",
+    "url": "/static/js/2.7c9eebd5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7c9eebd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7f747ff583fda38440",
+    "url": "/static/js/main.f05228e8.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
