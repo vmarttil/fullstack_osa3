f0720b1ba46639fe2da8cab97cee7f9490c82e50
Ville Marttila <ville.marttila@iki.fi>
2020-02-10 02:10:06 +0200

    tehtävä 3.3 tehty

--- index.js
+++ index.js
@@ -24,14 +24,25 @@ const persons = [
       }
     ]
 
-  app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.reduce((acc, person) => acc + 1, 0)} people</p>
-                    <p>${new Date()}</p>`)
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.reduce((acc, person) => acc + 1, 0)} people</p>
+                 <p>${new Date()}</p>`)
     
-  })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
+})
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    response.json(persons)
 })
 
 
